c309c3f91761cd892cab313730ab4205d1860e6f
shahab-github <113800863+shahab-github@users.noreply.github.com>
2024-10-31 20:56:54 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1.0",
+    description="A simple example Python package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/mypackage",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
